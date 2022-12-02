a9db7f13e5149ae09b67ed77ff55250f2c025bd8
tamirulalan <tamirulalan@gmail.com>
2022-12-02 11:04:38 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 //require the express module
 const express = require('express');
+const { createEngine } = require('express-react-views');
 const app = express();
 const port = 3000;
 
 const pokemon = require('./models/pokemon.js');
-
+//app.set('views', './views') 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+//sending a response in diffrent forms
 app.get('/', (req,res)=>{
   res.send('Welcome to the Pokemon App!')
 })
+//checking as my server readsmy database
 app.get('/pokemon', (req,res)=>{
     res.send(pokemon)
   })
+  
+  app.get(`/pokemon`, (req,res)=>{
+    res.render(`Index`, {pokemon:pokemon})
+})
+
+app.get(`/pokemon/:id`, (req,res)=>{
+  const {id} = req.params
+  res.render(`Show`, {pokemon:pokemon[id]})
+})
+
+
 app.listen(port,() => {
     console.log('listening on port' , port);
 });
---
+++ views/Index.jsx
@@ -0,0 +1,36 @@
+const React = require("react");
+
+
+function cap(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+
+class Index extends React.Component {
+  render() {
+    const { pokemon } = this.props;
+    return (
+      <div style={styles}>
+        <h1>See All The Pokemon</h1>
+          
+          {console.log(pokemon)}
+            <ul>
+             {pokemon.map((pokemon, idx) => (
+              <li key={idx}>
+                <img src={pokemon.img} />
+                <br/>
+                <a href={`/pokemon/${idx}`}>{cap(pokemon.name)}</a>
+              </li>
+            ))}
+          </ul>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  color: `#ffffff`,
+  backgroundColor: `#000000`,
+};
+
+module.exports = Index;
